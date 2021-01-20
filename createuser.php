b3ecad25c54c44c04c56199eaf614c0391bfcbf4
WhizzCodes <74658164+WhizzCodes@users.noreply.github.com>
2021-01-20 14:46:53 +0530

    Create createuser.php

---
+++ createuser.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="table.css">
+    <link rel="stylesheet" type="text/css" href="navbar.css">
+    <link rel="stylesheet" type="text/css" href="createuser.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+	.footer{
+	background-color:white;
+	
+}
+.footer p{
+	color:black;
+	
+}
+.fa:hover {
+    opacity: 0.7;
+	transform:scale(1.1);
+}
+.fa-facebook {
+  padding-top:50%;
+  background: #3B5998;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 70px;
+  text-align: center;
+  text-decoration: none;
+  transition:transform 0.2;
+  border-radius:50px;
+}
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 70px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  transition:transform 0.2;
+   border-radius:50px;
+}
+.fa-instagram {
+  background: #de1659;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 70px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  transition:transform 0.2;
+   border-radius:50px;
+}
+.fa-envelope-open {
+  background: #15ab38;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 70px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  transition:transform 0.2;
+   border-radius:50px;
+}
+</style>
+</head>
+
+<body>
+<?php
+    include 'config.php';
+    if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $balance=$_POST['balance'];
+    $sql="insert into users(name,email,balance) values('{$name}','{$email}','{$balance}')";
+    $result=mysqli_query($conn,$sql);
+    if($result){
+               echo "<script> alert('Hurray! User created');
+                               window.location='transfermoney.php';
+                     </script>";
+                    
+    }
+  }
+?>
+
+<?php
+  include 'navbar.php';
+?>
+
+        <h2 class="text-center pt-4">Create a New User</h2>
+        <br>
+
+  <div class="background">
+  <div class="container">
+    <div class="screen">
+      <div class="screen-header">
+        <div class="screen-header-right">
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+        </div>
+      </div>
+      <div class="screen-body">
+        <div class="screen-body-item left">
+          <img class="img-fluid" src="user3.jpg" style="border: none; border-radius: 50%;">
+        </div>
+        <div class="screen-body-item">
+          <form class="app-form" method="post">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="NAME" type="text" name="name" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="EMAIL" type="email" name="email" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="BALANCE" type="number" name="balance" required>
+            </div>
+            <br>
+            <div class="app-form-group button">
+              <input class="app-form-button" type="submit" value="CREATE" name="submit"></input>
+              <input class="app-form-button" type="reset" value="RESET" name="reset"></input>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+     <div class="footer">
+<br>
+<center><a href="https://m.facebook.com/create.whizz" class="fa fa-facebook" target="_blank"></a>
+<a href="https://www.instagram.com/create_whizz/" class="fa fa-instagram" target="_blank"></a>
+<a href="https://m.youtube.com/channel/UCYA1wcOPy2AgmGK2SPTurrg" class="fa fa-youtube" target="_blank"></a>
+<a href="mailto:whizz1026@gmail.com" class="fa fa-envelope-open" target="_blank"></a><br><br>
+<p><i class="fa fa-copyright"></i> 2021 tsffirstproject.000webhostapp.com</p><br><br></center>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
