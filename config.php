1562f37303e637f4c2e2f55538e639e891840742
WhizzCodes <74658164+WhizzCodes@users.noreply.github.com>
2021-01-20 14:45:12 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$conn = mysqli_connect('localhost','id15835830_simplebankingsystem123','2P^%D(W=Y4B!CtrN','id15835830_simplebankingsystem');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
