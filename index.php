477b65faccf0aee61f5811e63d1d1737f07787de
WhizzCodes <74658164+WhizzCodes@users.noreply.github.com>
2021-01-20 14:48:25 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="navbar.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Basic Banking System</title>
+	<style>
+	.footer{
+	background-color:white;
+	
+}
+.footer p{
+	color:black;
+	
+}
+.fa:hover {
+    opacity: 0.7;
+	transform:scale(1.1);
+}
+.fa-facebook {
+  padding-top:50%;
+  background: #3B5998;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 70px;
+  text-align: center;
+  text-decoration: none;
+  transition:transform 0.2;
+  border-radius:50px;
+}
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 70px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  transition:transform 0.2;
+   border-radius:50px;
+}
+.fa-instagram {
+  background: #de1659;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 70px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  transition:transform 0.2;
+   border-radius:50px;
+}
+.fa-envelope-open {
+  background: #15ab38;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 70px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  transition:transform 0.2;
+   border-radius:50px;
+}
+	</style>
+  </head>
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div class="row intro py-1">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h3>Welcome to</h3>
+                  <h1>TSF Bank</h1>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md img text-center">
+                <img src="bank.jpg" class="img-fluid pt-2">
+              </div>
+            </div>
+
+     <div class="footer">
+<br>
+<center><a href="https://m.facebook.com/create.whizz" class="fa fa-facebook" target="_blank"></a>
+<a href="https://www.instagram.com/create_whizz/" class="fa fa-instagram" target="_blank"></a>
+<a href="https://m.youtube.com/channel/UCYA1wcOPy2AgmGK2SPTurrg" class="fa fa-youtube" target="_blank"></a>
+<a href="mailto:whizz1026@gmail.com" class="fa fa-envelope-open" target="_blank"></a><br><br>
+<p><i class="fa fa-copyright"></i> 2021 tsffirstproject.000webhostapp.com</p><br><br></center>
+</div>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
